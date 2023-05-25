0eac47038cdca98c970f43f49c84e6fbd32a7f5a
Frank Pereira <frankpereirag000@gmail.com>
2023-05-25 15:19:12 +0200

    Create generalError error

--- src/server/middlewares/errorMiddlewares/errorMiddleware.ts
+++ src/server/middlewares/errorMiddlewares/errorMiddleware.ts
@@ -1,6 +1,7 @@
 import createDebug from "debug";
 import { type NextFunction, type Request, type Response } from "express";
 import CustomError from "../../../CustomError/CustomError";
+import chalk from "chalk";
 
 const debug = createDebug("game-nest:server:middlewares:errorMiddleware");
 
@@ -11,7 +12,20 @@ export const notFoundError = (
 ) => {
   const error = new CustomError(404, "Endpoint not found");
 
-  debug(error);
-
   next(error);
+};
+
+export const generalError = (
+  error: CustomError,
+  req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  debug(`Error: ${chalk.red(error.message)}`);
+
+  const statusCode = error.statusCode || 500;
+
+  const message = error.statusCode ? error.message : "General error";
+
+  res.status(statusCode).json({ message });
 };
