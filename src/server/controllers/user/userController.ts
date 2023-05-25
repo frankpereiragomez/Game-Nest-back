a19dfb39428d07ae7f63060de2fe6c9cf5c4950b
Frank Pereira <frankpereirag000@gmail.com>
2023-05-25 21:46:20 +0200

    Create loginUser controller

---
+++ src/server/controllers/user/userController.ts
@@ -0,0 +1,37 @@
+import jwt, { type JwtPayload } from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import { type NextFunction, type Response } from "express";
+import { type UserCredentialsRequest } from "../../types";
+import User from "../../../database/models/User";
+import CustomError from "../../../CustomError/CustomError";
+
+export const loginUser = async (
+  req: UserCredentialsRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username }).exec();
+
+  try {
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      const error = new CustomError(401, "Wrong credentials");
+
+      throw error;
+    }
+
+    const tokenPayload: JwtPayload = {
+      sub: user._id.toString(),
+      name: user.name,
+    };
+
+    const token = jwt.sign(tokenPayload, process.env.JWT_SECRET!, {
+      expiresIn: "7d",
+    });
+
+    res.status(200).json({ token });
+  } catch (error) {
+    next(error);
+  }
+};
