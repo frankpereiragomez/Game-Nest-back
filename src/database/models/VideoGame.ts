09c7f04ec6d094de097bc62bce92fe18af23d2fc
Frank Pereira <frankpereirag000@gmail.com>
2023-05-26 19:45:11 +0200

    Add videogameSchemaand Videogame model

---
+++ src/database/models/VideoGame.ts
@@ -0,0 +1,43 @@
+import { Schema, Types, model } from "mongoose";
+import User from "./User";
+
+const videogameSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  genre: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  pegi: {
+    type: String,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  developers: {
+    type: String,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  videoGameCompany: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: Types.ObjectId,
+    ref: User,
+  },
+});
+
+const Videogame = model("Videogame", videogameSchema, "videogames");
+
+export default Videogame;
